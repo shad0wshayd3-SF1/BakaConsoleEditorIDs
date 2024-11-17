8a41c32eeb9e2f2c9f01003ca2b8f61324e73b8b
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-16 18:33:26 -0700

    refactor: cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
-class Hooks
+namespace Hooks
 {
-public:
-	static void Install()
+	namespace hkGetReferencedObject
 	{
-		hkGetReferencedObject::Install();
-	}
+		static RE::TESForm* GetReferencedObject(RE::SCRIPT_WORD* a_currentWord)
+		{
+			auto form = RE::TESForm::LookupByEditorID(a_currentWord->text);
+			if (form && form->formType != RE::FormType::kGMST)
+			{
+				return form;
+			}
+			else if (auto formID = std::strtoul(a_currentWord->text, nullptr, 16))
+			{
+				return RE::TESForm::LookupByID(formID);
+			}
 
-private:
-	class hkGetReferencedObject
-	{
-	public:
+			return nullptr;
+		}
+
 		static void Install()
 		{
 			struct GetReferencedObjectPatch : Xbyak::CodeGenerator
 			{
-			public:
 				GetReferencedObjectPatch(std::uintptr_t a_func, std::uintptr_t a_retn)
 				{
 					Xbyak::Label func;
@@ -35,36 +41,26 @@ private:
 				}
 			};
 
-			static REL::Relocation<std::uintptr_t> target{ REL::ID(109296) };
+			static REL::Relocation target{ REL::ID(109296) };
 			static constexpr auto TARGET_ADDR{ 0x14D };
 			static constexpr auto TARGET_RETN{ 0x17C };
 			static constexpr auto TARGET_FILL{ TARGET_RETN - TARGET_ADDR };
 			REL::safe_fill(target.address() + TARGET_ADDR, REL::NOP, TARGET_FILL);
 
-			auto code = GetReferencedObjectPatch(
+			auto code = GetReferencedObjectPatch{
 				reinterpret_cast<std::uintptr_t>(GetReferencedObject),
-				target.address() + TARGET_RETN);
+				target.address() + TARGET_RETN
+			};
 			auto& trampoline = SFSE::GetTrampoline();
 			trampoline.write_branch<5>(target.address() + TARGET_ADDR, trampoline.allocate(code));
 		}
+	}
 
-	private:
-		static RE::TESForm* GetReferencedObject(RE::SCRIPT_WORD* a_currentWord)
-		{
-			auto form = RE::TESForm::LookupByEditorID(a_currentWord->text);
-			if (form && form->formType != RE::FormType::kGMST)
-			{
-				return form;
-			}
-			else if (auto formID = std::strtoul(a_currentWord->text, nullptr, 16))
-			{
-				return RE::TESForm::LookupByID(formID);
-			}
-
-			return nullptr;
-		}
-	};
-};
+	static void Install()
+	{
+		hkGetReferencedObject::Install();
+	}
+}
 
 namespace
 {
@@ -87,7 +83,7 @@ SFSEPluginLoad(const SFSE::LoadInterface* a_sfse)
 {
 	SFSE::Init(a_sfse);
 
-	SFSE::AllocTrampoline(64);
+	SFSE::AllocTrampoline(16);
 	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
 
 	return true;
